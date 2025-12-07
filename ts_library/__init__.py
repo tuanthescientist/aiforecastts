4a1938922793d7ac1aba04c8c3922d910f4c11cc
Tuan Anh Tran <tuanthescientist@gmail.com>
2025-12-07 17:36:40 +0700

    Fix CI YAML indentation + deps (gcc g++ python3-dev), export SuperForecaster, upgrade pip in CI

--- ts_library/__init__.py
+++ ts_library/__init__.py
@@ -1,4 +1,4 @@
-from .core import TimeSeriesAnalyzer
+from .core import TimeSeriesAnalyzer, SuperForecaster
 
 __version__ = "0.1.0"
-__all__ = ["TimeSeriesAnalyzer"]
+__all__ = ["TimeSeriesAnalyzer", "SuperForecaster"]
